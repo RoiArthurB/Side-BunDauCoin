756c9fdd660bba9fca1df254b0f3b990619b68b1
RoiArthurB <contact@arthurbrugiere.fr>
2021-06-10 23:17:58 +0700

    [BC] Add tools to verify bc

--- BunDauCoin/blockchain.py
+++ BunDauCoin/blockchain.py
@@ -43,3 +43,46 @@ def generateNextBlock(blockData):
         timestamp=datetime.datetime.now(), 
         data=blockData
     )
+
+"""
+    +=============================+
+    |   Checking the Blockchain   |
+    |    integrity                |
+    +=============================+
+"""
+
+##
+## Determines if valid new block.
+##
+## :param      newBlock:       The new block
+## :type       newBlock:       { type_description }
+## :param      previousBlock:  The previous block
+## :type       previousBlock:  { type_description }
+##
+## :returns:   True if valid new block, False otherwise.
+## :rtype:     bool
+##
+def isValidNewBlock(newBlock, previousBlock):
+    result = False
+    if previousBlock.index + 1 == newBlock.index:
+        if previousBlock.hash == newBlock.previousHash:
+            if block.Block.calculateHash(newBlock) == newBlock.hash:
+                result = True
+    
+    return result
+##
+## Determines whether the specified block is valid block structure.
+##
+## :param      block:  The block
+## :type       block:  { type_description }
+##
+## :returns:   True if the specified block is valid block structure, False otherwise.
+## :rtype:     bool
+##
+def isValidBlockStructure(block):
+    return (isinstance(block.index, int) 
+        and isinstance(block.hash, str) 
+        and isinstance(block.previousHash, str) 
+        and isinstance(block.timestamp, datetime.datetime)
+        and isinstance(block.data, str)
+    )

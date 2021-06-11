b786b637b1fcf377176c7fa469460f02938cd453
RoiArthurB <contact@arthurbrugiere.fr>
2021-06-11 21:18:05 +0700

    [DB] Fix columns creationg pb Remove 'index' and duplicated 'previousHash' + Use allowed name 'timestamp'

--- BunDauCoin/schema.sql
+++ BunDauCoin/schema.sql
@@ -2,10 +2,8 @@ DROP TABLE IF EXISTS bun_dau_blockchain;
 
 CREATE TABLE bun_dau_blockchain (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  index TEXT NOT NULL,
   previousHash TEXT NOT NULL,
-  previousHash TEXT NOT NULL,
-  timest TEXT NOT NULL,
+  timestamp TEXT NOT NULL,
   data TEXT,
   hash TEXT NOT NULL
 );

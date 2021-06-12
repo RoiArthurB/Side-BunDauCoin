b0fbe394fd5af3421d94f2f68d5a6583025b558b
RoiArthurB <contact@arthurbrugiere.fr>
2021-06-12 15:58:03 +0700

    [BLOCK] Hash parameter optional in builder Allow to create Block object from bdd + Full class comment

--- BunDauCoin/block.py
+++ BunDauCoin/block.py
@@ -2,15 +2,40 @@ from flask import current_app, g
 
 import hashlib
 
+##
+## This class describes a block.
+##
 class Block:
-
-    def __init__(self, index: int, previousHash: str, timestamp, data: str):
+    ##
+    ## Constructs a new instance.
+    ##
+    ## :param      index:         The index
+    ## :type       index:         int
+    ## :param      previousHash:  The previous hash
+    ## :type       previousHash:  str
+    ## :param      timestamp:     The timestamp
+    ## :type       timestamp:     datetime.datetime
+    ## :param      data:          The data
+    ## :type       data:          str
+    ## :param      hash:          The hash
+    ## :type       hash:          str
+    ##
+    def __init__(self, index: int, previousHash: str, timestamp, data: str, hash: str = ""):
         self.index = index
         self.previousHash = previousHash
         self.timestamp = timestamp
         self.data = data
-        self.hash = self.calculateHash()
+        if hash == "":
+            self.hash = self.calculateHash()
+        else:
+            self.hash = hash
 
+    ##
+    ## Calculates the hash.
+    ##
+    ## :returns:   The hash.
+    ## :rtype:     str
+    ##
     def calculateHash(self) -> str:
         sha = hashlib.sha256()
         sha.update(str(self.index).encode('utf-8') + str(self.previousHash).encode('utf-8') + str(self.timestamp).encode('utf-8') + str(self.data).encode('utf-8'))

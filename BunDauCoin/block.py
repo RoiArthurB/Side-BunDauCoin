42dd3f45a5e9523079007ce7935d1abbe228dc49
RoiArthurB <contact@arthurbrugiere.fr>
2021-06-13 18:36:41 +0700

    [BLOCK] Add getters

--- BunDauCoin/block.py
+++ BunDauCoin/block.py
@@ -40,3 +40,53 @@ class Block:
         sha = hashlib.sha256()
         sha.update(str(self.index).encode('utf-8') + str(self.previousHash).encode('utf-8') + str(self.timestamp).encode('utf-8') + str(self.data).encode('utf-8'))
         return sha.hexdigest()
+
+    """
+        +=====================+
+        |   Getter / Setter   |
+        +=====================+
+    """
+    ##
+    ## Gets the index.
+    ##
+    ## :returns:   The index.
+    ## :rtype:     int
+    ##
+    def getIndex(self) -> int:
+        return int(self.index)
+    
+    ##
+    ## Gets the previous hash.
+    ##
+    ## :returns:   The previous hash.
+    ## :rtype:     str
+    ##
+    def getPreviousHash(self) -> str:
+        return self.previousHash
+    
+    ##
+    ## Gets the hash.
+    ##
+    ## :returns:   The hash.
+    ## :rtype:     str
+    ##
+    def getHash(self) -> str:
+        return self.hash
+    
+    ##
+    ## Gets the timestamp.
+    ##
+    ## :returns:   The timestamp.
+    ## :rtype:     int
+    ##
+    def getTimestamp(self) -> int:
+        return self.timestamp
+    
+    ##
+    ## Gets the data.
+    ##
+    ## :returns:   The data.
+    ## :rtype:     str
+    ##
+    def getData(self) -> str:
+        return self.data
